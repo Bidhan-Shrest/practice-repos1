c5a9010c3f6469c726edeb5568e0f5460483a8bc
Bidhan-Shrest <xthabidhan57@gmail.com>
2023-12-19 22:30:49 +0545

    const prompt if else alert

---
+++ practice.js
@@ -0,0 +1,12 @@
+// const name = prompt('what is your name')
+// const age = prompt('what is your age')
+
+// console.log(name, age)
+
+// if (name=== 'bidhan'){
+//     alert('welcome sir')
+// }
+// else {
+//     alert('reenter again')
+// }
+
