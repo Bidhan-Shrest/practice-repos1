2c4d274e64b8c8abc2cea0529158442d48b8acf1
Bidhan-Shrest <xthabidhan57@gmail.com>
2023-12-20 17:58:15 +0545

    operation.js

---
+++ operations-ststement/operations.js
@@ -0,0 +1,33 @@
+console.log("hello yaara")
+/*
+logical operation
+
+-and (&&)
+-or (||)
+-not (!)
+*/
+
+const first = Number(prompt("Enter number between 1 to 10")); //"8"
+const second = prompt("enter second number") ; //"8"
+const ans = Number(first) + Number(second);
+console.log(first, second);
+if (first>second){
+    alert(ans);
+}
+else {
+    alert(first * second)
+}
+
+if (first>second && second==8){
+    alert("hello ")
+}
+else {
+    alert("bye")
+}
+
+if (first<second || second>5){
+    alert("milyo")
+}
+else{
+    alert("milena")
+}
