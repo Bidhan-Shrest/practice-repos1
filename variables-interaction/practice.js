9cef2dacbb1c62b454cde884ae26f287eac4a3bc
Bidhan-Shrest <xthabidhan57@gmail.com>
2023-12-20 17:18:20 +0545

    creating operations-ststement/index.js and linking to index.html

---
+++ variables-interaction/practice.js
@@ -0,0 +1,12 @@
+ let name = prompt('what is your name')
+ const age = prompt('what is your age')
+
+ console.log(name, age)
+
+ if (name=='bidhan'){
+     alert('welcome sir')
+ }
+ else {
+     alert('reenter again')
+}
+
