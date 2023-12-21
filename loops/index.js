21b535c419e02e09cac3f6d5a73eaa0909aa24e3
Bidhan-Shrest <xthabidhan57@gmail.com>
2023-12-21 18:08:57 +0545

    for, while, do while loops

--- loops/index.js
+++ loops/index.js
@@ -1,5 +1,28 @@
-//ternary operator
-const age=Number(prompt("enter your age"));
+//ternary operator replacement of if else condition
+// const age=Number(prompt("enter your age"));
 // let age = 12;
-const result =
-    age >= 18? alert("you can vote") : alert("you cannot vote");
+// const result =
+    // age >= 18? alert("you can vote") : alert("you cannot vote");
+
+
+    //loops
+
+    //write a loop that prints from 1 to 100
+
+    // for (let a = 1; a <= 100; a++){
+    //     console.log(a);
+    // }
+
+    
+// let i = 1;
+//     while (i <= 10) {
+        
+//         console.log(i)
+//         i++;
+//       }
+let i = 1;
+do {
+    console.log(2 * i);
+    i++;
+  }
+  while (i <= 10);
