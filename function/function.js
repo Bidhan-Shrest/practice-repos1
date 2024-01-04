223f7ae540d6a8989f8b4b4eabc3ae0abaa3fac5
Bidhan-Shrest <xthabidhan57@gmail.com>
2024-01-04 18:07:03 +0545

    function

--- function/function.js
+++ function/function.js
@@ -1,11 +1,35 @@
-const num = Number(prompt("give me a number"));
-let i = 1;
-const mult = (num) => {
-    return num * i;
-};
+// const num = Number(prompt("give me a number"));
+// let i = 1;
+// const mult = (num) => {
+//     return num * i;
+// };
 
-while (i <= 10) {
-    alert(mult(num));
-    i++;
-}
+// while (i <= 10) {
+//     alert(mult(num));
+//     i++;
+// }
 
+// const pal = number(prompt("enter"));
+// const drome = 
+
+//write a function that return name who has admin access.
+// const users = [
+//     {name:"raktim", roles:["admin"]},
+//     {name:"harry", roles:["guest"] },
+//     {name:"sita", roles:["admin", "vendor"]}
+// ]
+// const checkRole=(role) =>{
+// const newUsers = users.map((user) => {
+//     if (user.roles.includes(role)) return user.name;
+// })
+// return newUsers;
+// return users.filter((user) => user.roles.includes(roles));
+// }
+// const resp = checkRole(prompt("role"));
+// console.log(resp)
+
+//write a function that sum every character of [],2,3]
+
+const myar = [1, 2, 3];
+const int = 0;
+const sum = num
