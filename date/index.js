9d7be4c6470636df8dc5e8364912870e41c2b737
Bidhan-Shrest <xthabidhan57@gmail.com>
2024-01-05 17:33:27 +0545

    date and html-basics

---
+++ date/index.js
@@ -0,0 +1,21 @@
+console.log("hello from date");
+
+//date is an object
+
+// const today = new Date();//default current date and time
+// const someTime = new Date("2016-01-02");
+// console.log({today, someTime});
+
+
+//write a function that accepts the date and shows day as entered.
+
+
+const date =new Date(prompt("year, month, day")) ;
+
+
+    console.log(date.toDateString());
+
+
+
+
+
