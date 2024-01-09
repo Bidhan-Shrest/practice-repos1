0352f84e478d6b5ca871c9103e4a0adaaf7854a8
Bidhan-Shrest <xthabidhan57@gmail.com>
2024-01-09 16:50:35 +0545

    added next.html and used card in html

--- html-basics/index.js
+++ html-basics/index.js
@@ -1,6 +1,6 @@
 function getFormData() {
     const fName = document.getElementById("fName").value;
-    // const mName = document.getElementById("mName").Value;
+    const mName = document.getElementById("mName").Value;
     const lName = document.getElementById("lName").value;
 
     console.log({ fName, lName });
